7f9dc81e155a06a0e6d9157112c082c3393f62da
Qanshin <qanshin.info@gmail.com>
2018-07-23 21:23:31 +0400

    Second commit

---
+++ contact.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Քանշին | Շինարարություն և Վերանորոգում</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="images/favicon.png" type="image/x-icon" />
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Animate CSS -->
+    <link href="vendors/animate/animate.css" rel="stylesheet">
+    <!-- Icon CSS-->
+	<link rel="stylesheet" href="vendors/font-awesome/css/font-awesome.min.css">
+    <!-- Camera Slider -->
+    <link rel="stylesheet" href="vendors/camera-slider/camera.css">
+    <!-- Owlcarousel CSS-->
+	<link rel="stylesheet" type="text/css" href="vendors/owl_carousel/owl.carousel.css" media="all">
+
+    <!--Theme Styles CSS-->
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.min.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+    <meta charset="UTF-8">
+</head>
+<body>
+    <!-- Preloader -->
+    <div class="preloader"></div>
+
+	<!-- Top Header_Area -->
+	<section class="top_header_area">
+	    <div class="container">
+            <ul class="nav navbar-nav top_nav">
+                <li><a href="#"><i class="fa fa-phone"></i>+373 93 268 925</a></li>
+                <li><a href="#"><i class="fa fa-envelope-o"></i>qanshin.info@gmail.com</a></li>
+                <li><a href="#"><i class="fa fa-clock-o"></i>Երկ - Ուրբ 9:00 - 20:00</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right social_nav">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+            </ul>
+	    </div>
+	</section>
+	<!-- End Top Header_Area -->
+
+	<!-- Header_Area -->
+    <nav class="navbar navbar-default header_aera" id="main_navbar">
+        <div class="container">
+            <!-- searchForm -->
+            <div class="searchForm">
+                <form action="#" class="row m0">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                        <input type="search" name="search" class="form-control" placeholder="Type & Hit Enter">
+                        <span class="input-group-addon form_hide"><i class="fa fa-times"></i></span>
+                    </div>
+                </form>
+            </div><!-- End searchForm -->
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="col-md-2 p0">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#min_navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"><img src="images/logo1.png" alt=""></a>
+                </div>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="col-md-10 p0">
+                <div class="collapse navbar-collapse" id="min_navbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown submenu">
+                            <li><a href="#">Գլխավոր</a></li>
+                        </li>
+                        <li class="dropdown submenu">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Ծառայություններ</a>
+                            <ul class="dropdown-menu other_dropdwn">
+                                <li><a href="renovation.html">Տան Վերանորոգում</a></li>
+                                <li><a href="service.html">Տուն Սերվիս</a></li>
+                                <li><a href="design.html">Դիզայներական Ծառայություններ</a></li>
+                            </ul>
+                        </li>
+                        <!--<li class="dropdown submenu">
+                            <li><a href="#">Գործընկեր</a></li>
+                        </li>-->
+                        <li><a href="about.html">Մեր Մասին</a></li>
+                        <li><a href="contact.html">Հետադարձ Կապ</a></li>
+                        <!-- <li><a href="#" class="nav_searchFrom"><i class="fa fa-search"></i></a></li> -->
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div>
+        </div><!-- /.container -->
+    </nav>
+	<!-- End Header_Area -->
+
+    <!-- Banner area -->
+    <section class="banner_area" data-stellar-background-ratio="0.5">
+        <h2>Հետադարձ Կապ</h2>
+        <ol class="breadcrumb">
+            <li><a href="index.html">Գլխավոր</a></li>
+            <li><a href="#" class="active">Հետադարձ Կապ</a></li>
+        </ol>
+    </section>
+    <!-- End Banner area -->
+
+    <!-- Map -->
+    <div class="contact_map">
+
+    </div>
+    <!-- End Map -->
+
+    <!-- All contact Info -->
+    <section class="all_contact_info">
+        <div class="container">
+            <div class="row contact_row">
+                <div class="col-sm-6 contact_info">
+                    <h2>Հետադարծ Կապ</h2>
+                    <p>Համագործակցության, հարցերի կամ առաջարկությունների համար մեզ հետ կարող եք կապնվել հետևյալ կապի միջոցներով կամ ուղարկել նամակ </p>
+                    <div class="location">
+                        <div class="location_laft">
+                            <a href="#">հասցե</a>
+                            <a href="#">հեռ.</a>
+                            <a href="#">էլ․ հասցե</a>
+                        </div>
+                        <div class="address">
+                            <a href="#">Պուշկինի փողոց, ք․ Երևան, Հայաստան 0019</a>
+                            <a href="#">+374 93 268 925</a>
+                            <a href="#">qanshin.info@gmail.com</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 contact_info send_message">
+                    <h2>Կապնվեք Մեզ Հետ</h2>
+                    <form class="form-inline contact_box" method="post">
+                        <input name="name" type="text" class="form-control input_box" placeholder="Անուն *">
+                        <input name="phone" type="text" class="form-control input_box" placeholder="Հեռախոսահամար *">
+                        <textarea name="message" class="form-control input_box" placeholder="Նամակ"></textarea>
+                        <input name="send" type="submit" class="btn btn-default" value="Ուղարկել">
+                    </form>
+                    <?php
+                    $name = $_POST['name'];
+                    $phone = $_POST['phone'];
+                    $message = $_POST['message'];
+
+                    $to      = 'qanshin.info@gmail.com';
+                    $subject = 'Message from'.$name;
+                    $headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+                    ?>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End All contact Info -->
+
+    <!-- Footer Area -->
+    <footer class="footer_area">
+        <div class="container">
+            <div class="footer_row row">
+                <div class="col-md-3 col-sm-6 footer_about">
+                    <h2>ABOUT OUR COMPANY</h2>
+                    <img src="images/footer-logo.png" alt="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <ul class="socail_icon">
+                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-6 footer_about quick">
+                    <h2>Quick links</h2>
+                    <ul class="quick_link">
+                        <li><a href="#"><i class="fa fa-chevron-right"></i>Building Construction</a></li>
+                        <li><a href="#"><i class="fa fa-chevron-right"></i>Home Renovation</a></li>
+                        <li><a href="#"><i class="fa fa-chevron-right"></i>Hardwood Flooring</a></li>
+                        <li><a href="#"><i class="fa fa-chevron-right"></i>Repairing Of Roof</a></li>
+                        <li><a href="#"><i class="fa fa-chevron-right"></i>Commercial Construction</a></li>
+                        <li><a href="#"><i class="fa fa-chevron-right"></i>Concreate Transport</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-6 footer_about">
+                    <h2>Twitter Feed</h2>
+                    <a href="#" class="twitter">@colorlib: Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</a>
+                    <a href="#" class="twitter">@colorlib: Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat.</a>
+                </div>
+                <div class="col-md-3 col-sm-6 footer_about">
+                    <h2>CONTACT US</h2>
+                    <address>
+                        <p>Have questions, comments or just want to say hello:</p>
+                        <ul class="my_address">
+                            <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i>info@thethemspro.com</a></li>
+                            <li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i>+880 123 456 789</a></li>
+                            <li><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i><span>Sector # 10, Road # 05, Plot # 31, Uttara, Dhaka 1230 </span></a></li>
+                        </ul>
+                    </address>
+                </div>
+            </div>
+        </div>
+        <div class="copyright_area">
+            CCopyright 2017 All rights reserved. Designed by <a href="https://colorlib.com">Colorlib.</a>
+        </div>
+    </footer>
+    <!-- End Footer Area -->
+
+    <!-- jQuery JS -->
+    <script src="js/jquery-1.12.0.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Animate JS -->
+    <script src="vendors/animate/wow.min.js"></script>
+    <!-- Camera Slider -->
+    <script src="vendors/camera-slider/jquery.easing.1.3.js"></script>
+    <script src="vendors/camera-slider/camera.min.js"></script>
+    <!-- Isotope JS -->
+    <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+    <script src="vendors/isotope/isotope.pkgd.min.js"></script>
+    <!-- Progress JS -->
+    <script src="vendors/Counter-Up/jquery.counterup.min.js"></script>
+    <script src="vendors/Counter-Up/waypoints.min.js"></script>
+    <!-- Owlcarousel JS -->
+    <script src="vendors/owl_carousel/owl.carousel.min.js"></script>
+    <!-- Stellar JS -->
+    <script src="vendors/stellar/jquery.stellar.js"></script>
+    <!-- Map JS -->
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="vendors/map/topbuilder_map.min.js"></script>
+    <!-- Theme JS -->
+    <script src="js/theme.js"></script>
+</body>
+</html>
---
+++ design.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Քանշին | Շինարարություն և Վերանորոգում</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="images/favicon.png" type="image/x-icon" />
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Animate CSS -->
+    <link href="vendors/animate/animate.css" rel="stylesheet">
+    <!-- Icon CSS-->
+	<link rel="stylesheet" href="vendors/font-awesome/css/font-awesome.min.css">
+    <!-- Camera Slider -->
+    <link rel="stylesheet" href="vendors/camera-slider/camera.css">
+    <!-- Owlcarousel CSS-->
+	<link rel="stylesheet" type="text/css" href="vendors/owl_carousel/owl.carousel.css" media="all">
+
+    <!--Theme Styles CSS-->
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all" />
+
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.min.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+    <meta charset="UTF-8">
+</head>
+<body>
+    <!-- Preloader -->
+    <div class="preloader"></div>
+
+	<!-- Top Header_Area -->
+	<section class="top_header_area">
+	    <div class="container">
+            <ul class="nav navbar-nav top_nav">
+                <li><a href="#"><i class="fa fa-phone"></i>+373 93 268 925</a></li>
+                <li><a href="#"><i class="fa fa-envelope-o"></i>qanshin.info@gmail.com</a></li>
+                <li><a href="#"><i class="fa fa-clock-o"></i>Երկ - Ուրբ 9:00 - 20:00</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right social_nav">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+            </ul>
+	    </div>
+	</section>
+	<!-- End Top Header_Area -->
+
+	<!-- Header_Area -->
+    <nav class="navbar navbar-default header_aera" id="main_navbar">
+        <div class="container">
+            <!-- searchForm -->
+            <div class="searchForm">
+                <form action="#" class="row m0">
+                    <div class="input-group">
+                        <span class="input-group-addon"><i class="fa fa-search"></i></span>
+                        <input type="search" name="search" class="form-control" placeholder="Type & Hit Enter">
+                        <span class="input-group-addon form_hide"><i class="fa fa-times"></i></span>
+                    </div>
+                </form>
+            </div><!-- End searchForm -->
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="col-md-2 p0">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#min_navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"><img src="images/logo1.png" alt=""></a>
+                </div>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="col-md-10 p0">
+                <div class="collapse navbar-collapse" id="min_navbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown submenu">
+                            <li><a href="#">Գլխավոր</a></li>
+                        </li>
+                        <li class="dropdown submenu">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Ծառայություններ</a>
+                            <ul class="dropdown-menu other_dropdwn">
+                                <li><a href="renovation.html">Տան Վերանորոգում</a></li>
+                                <li><a href="service.html">Տուն Սերվիս</a></li>
+                                <li><a href="design.html">Դիզայներական Ծառայություններ</a></li>
+                            </ul>
+                        </li>
+                        <!--<li class="dropdown submenu">
+                            <li><a href="#">Գործընկեր</a></li>
+                        </li>-->
+                        <li><a href="about.html">Մեր Մասին</a></li>
+                        <li><a href="contact.html">Հետադարձ Կապ</a></li>
+                        <!-- <li><a href="#" class="nav_searchFrom"><i class="fa fa-search"></i></a></li> -->
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div>
+        </div><!-- /.container -->
+    </nav>
+	<!-- End Header_Area -->
+
+
+    <!-- Banner area -->
+    <section class="banner_area" data-stellar-background-ratio="0.5">
+        <h2>Դիզայներական ծառայություններ</h2>
+        <ol class="breadcrumb">
+            <li><a href="index.html">Ծառայություններ</a></li>
+            <li><a href="#" class="active">Դիզայներական ծառայություններ</a></li>
+        </ol>
+    </section>
+    <!-- End Banner area -->
+
+    <!-- About Us Area -->
+    <section class="about_us_area row">
+        <div class="container">
+            <div class="row about_row">
+                <div class="col-md-5 col-sm-6 about_client about_pages_client">
+                    <img src="images/cns-2.jpg" alt="">
+                </div>
+                <div class="who_we_area col-md-7 col-sm-6">
+                    <div class="subtittle">
+                        <h2>Դիզայներական ծառայություններ</h2>
+                    </div>
+                    <p>«Քանշին» Ընկերությունը նաև մատուցում է դիզայներական ծառայություններ։ Մեզ կարող եք վստահել ձեր բնակարանի կամ գրասենյակի գծագրական ինչպես նաև ինտերիոր դիզայնի աշխատանքները</p>
+                    <a href="contact.html" class="button_all">Կապնվել</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End About Us Area -->
+
+    <!-- call Area -->
+    <section class="call_min_area">
+        <div class="container">
+            <h2>+374 93 268 925</h2>
+            <p>Վստահեք ձեր տան կամ գրասենյակի վերանորոգումը մեզ։ Բարձր որակը երաշխավորված է</p>
+        </div>
+    </section>
+    <!-- End call Area -->
+
+        <!-- Footer Area -->
+        <footer class="footer_area">
+            <div class="container">
+                <div class="footer_row row">
+                    <div class="col-md-3 col-sm-6 footer_about">
+                        <h2>Քանշին ՍՊԸ</h2>
+                        <img src="images/footer1.png" alt="">
+                        <p>Մատչելի և որակյալ շինարարական և դիզայներական ծառայություններ</p>
+                        <ul class="socail_icon">
+                            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                            <li><a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 col-sm-6 footer_about quick">
+                        <h2>ՄԵՆՅՈՒ</h2>
+                        <ul class="quick_link">
+                            <li><a href="#"><i class="fa fa-chevron-right"></i>Տան Վերանորոգում</a></li>
+                            <li><a href="#"><i class="fa fa-chevron-right"></i>Տուն Սերվիս</a><
+                            <li><a href="#"><i class="fa fa-chevron-right"></i>Դիզայն</a></li>
+                            <li><a href="#"><i class="fa fa-chevron-right"></i>Գործընկեր</a></li>
+                            <li><a href="#"><i class="fa fa-chevron-right"></i>Մեր Մասին</a></li>
+                            <li><a href="#"><i class="fa fa-chevron-right"></i>Հետադարծ Կապ</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3 col-sm-6 footer_about">
+                        <h2>ԿԱՊՆՎԵՔ ՄԵԶ ՀԵՏ</h2>
+                        <address>
+                            <p>Հարցերի և համագործակցելու համար կարող եք կապնվել մեզ հետ</p>
+                            <ul class="my_address">
+                                <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i>info@qanshin.am</a></li>
+                                <li><a href="#"><i class="fa fa-phone" aria-hidden="true"></i>+374 93 268 925</a></li>
+                                <li><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i><span>Sector # 10, Road # 05, Plot # 31, Uttara, Dhaka 1230 </span></a></li>
+                            </ul>
+                        </address>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright_area">
+                2018 Քանշին ՍՊԸ. © Բոլոր իրավունքները պաշտպանված են</a>
+            </div>
+        </footer>
+        <!-- End Footer Area -->
+
+        <!-- jQuery JS -->
+        <script src="js/jquery-1.12.0.min.js"></script>
+        <!-- Bootstrap JS -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Animate JS -->
+        <script src="vendors/animate/wow.min.js"></script>
+        <!-- Camera Slider -->
+        <script src="vendors/camera-slider/jquery.easing.1.3.js"></script>
+        <script src="vendors/camera-slider/camera.min.js"></script>
+        <!-- Isotope JS -->
+        <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+        <script src="vendors/isotope/isotope.pkgd.min.js"></script>
+        <!-- Progress JS -->
+        <script src="vendors/Counter-Up/jquery.counterup.min.js"></script>
+        <script src="vendors/Counter-Up/waypoints.min.js"></script>
+        <!-- Owlcarousel JS -->
+        <script src="vendors/owl_carousel/owl.carousel.min.js"></script>
+        <!-- Stellar JS -->
+        <script src="vendors/stellar/jquery.stellar.js"></script>
+        <!-- Theme JS -->
+        <script src="js/theme.js"></script>
+    </body>
+    </html>
